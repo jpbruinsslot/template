405d67f96f9f95c784961bbfea30f44276621617
erroneousboat <jpbruinsslot@gmail.com>
2021-11-21 13:17:51 +0100

    Update help text

--- template.go
+++ template.go
@@ -24,22 +24,22 @@ USAGE:
 
 EXAMPLES:
 
-	# Use system wide environment variables
+    # Use system wide environment variables
     $ template -t input.tpml -o output.txt
     $ template -t input.tmpl > output.txt
-
-	# Use data files (support for env and json files)
+    
+    # Use data files (support for env and json files)
     $ template -t input.tpml -o output.txt -d data.env
     $ template -t input.tpml -o output.txt -d data.json
-
-	# Use stdin for template file
+    
+    # Use stdin for template file
     $ cat input.tmpl | template -o output.txt    # output txt file
-	$ cat input.tmpl | template -o -             # output to stdout
+    $ cat input.tmpl | template -o -             # output to stdout
     $ cat input.tmpl | template                  # output to stdout
-
-	# Use stdin for data file
-	$ cat data.env | template -t input.tmpl -o output.txt -d -  # output txt file
-	$ cat data.env | template -t input.tmpl -d -                # output to stdout
+    
+    # Use stdin for data file
+    $ cat data.env | template -t input.tmpl -o output.txt -d -  # output txt file
+    $ cat data.env | template -t input.tmpl -d -                # output to stdout
 
 VERSION:
     %s
@@ -50,7 +50,7 @@ WEBSITE:
 GLOBAL OPTIONS:
     -t, -template [template-file]    template file
     -o, -output [output-file]        output file
-	-d, -data [data-file]            data file
+    -d, -data [data-file]            data file
     -h, -help
 `
 )
